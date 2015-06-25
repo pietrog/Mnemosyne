32f630e09c9c078b0f5ff3e79c60f50b331291c0
pietroXPS <pierre.gaulard@gmail.com>
2015-06-25 13:44:34 +0200

    Add todo task, the most important: implement unit tests over memorisation process

--- app/src/main/java/com/mnemo/pietro/mnemosyne/MnemoCentral.java
+++ app/src/main/java/com/mnemo/pietro/mnemosyne/MnemoCentral.java
@@ -15,10 +15,11 @@ import model.dictionary.Global;
 import model.dictionary.tools.MnemoDBHelper;
 
 //EN COURS
-//@TODO: implementer un rappel basé sur l'alarme
-//@TODO: retirer les mots revises de la todays list
+//@TODO: VERY HIGH !!! implementer les tests unitaires sur la partie memorisation
 
 //A FAIRE
+//@TODO: implementer un rappel basé sur l'alarme
+//@TODO: retirer les mots revises de la todays list
 //@TODO: definir une regle de navigation dans l'appli (d'un dictionaire a l'autre, ...) coherente pour l'utilisateur
 //@TODO: manager l'initialisation/destruction de certaines ressources(dbhelpers, ...) dans MnemoCentral, rationaliser l'utilissation des dbhelpers
 //@TODO: implementer la modification des entites
